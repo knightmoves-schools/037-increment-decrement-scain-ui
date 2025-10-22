0eedd066e290d758a5164bf4fcfdb5eb293bdec6
scain-ui <scain@midland.k12.ia.us>
2025-10-22 15:32:10 +0000

    2

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ Document.getElementById("prefix-increment").innerHTML = ++count;
 Document.getElementById("before-postfix-increment").innerHTML = count;
 Document.getElementById("postfix-increment").innerHTML = count++;
 
-Document.getElementById("brefore-prefix-decrement").innerHTML = count;
+Document.getElementById("before-prefix-decrement").innerHTML = count;
 Document.getElementById("prefix-decrement").innerHTML = --count;
 
 Document.getElementById("before-postfix-decrement").innerHTML = count;
 Document.getElementById("postfix-decrement").innerHTML = count--;
+
 Document.getElementById("result").innerHTML = counter;
